8a84ee5a2cb19ac4b6fed79cd6c64d0a28e27159
eduar-martins <137216388+eduar-martins@users.noreply.github.com>
2024-09-08 19:10:51 -0300

    Add files via upload

---
+++ EstruturaDeDados2/rAAArrayStruct.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMITE 100
+
+typedef struct{
+    int level;
+    char nome[20];
+    double movimento;
+}Personagem;
+
+int main(){
+
+    FILE *arquivo;
+    Personagem party[3];
+    Personagem teste[3];
+    char nArquivo[20];
+
+    printf("Qual nome da campanha?\n");
+    scanf("%s", nArquivo);
+
+    for(int cont = 0; cont < LIMITE; cont++){
+        if(nArquivo[cont] == '\0'){
+            nArquivo[cont] = '.';
+            nArquivo[cont + 1] = 't';
+            nArquivo[cont + 2] = 'x';
+            nArquivo[cont + 3] = 't';
+            nArquivo[cont + 4] = '\0';
+            break;
+        }
+    }
+    if((arquivo = fopen(nArquivo, "w+")) == NULL){
+        printf("deu pau :(");
+        return 1;
+    }
+
+    for(int cont = 0; cont < 3; cont++){
+        printf("Qual nome do personagem %d\n", cont + 1);
+        scanf("%s", party[cont].nome);
+        printf("Qual level do personagem %s?\n", party[cont].nome);
+        scanf("%d", &party[cont].level);
+        printf("Quanto de movimento tem o personagem %s\n", party[cont].nome);
+        scanf("%lf", &party[cont].movimento);
+        if(fwrite(&party[cont], sizeof(Personagem), 1, arquivo) != 1){
+            printf("o personagem %d deu pau na escrita do arquivo D:", cont + 1);
+            return 1;
+        }
+    }
+    rewind(arquivo);
+    printf("\n\n\n\n\n\n");
+
+    if(fread(&teste, 3*sizeof(Personagem), 1, arquivo) != 1){
+        printf("Deu pau na leitura do arquivo D:");
+        return 1;
+    }
+    
+    for(int cont = 0; cont < 3; cont++){
+        printf("nome: %s\nlevel: %d\nmovimento: %.1lf\n\n", teste[cont].nome, teste[cont].level, teste[cont].movimento);
+    }
+    
+    fclose(arquivo);
+
+    return 0;
+}
