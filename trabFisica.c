90f3af3b8be4f0d15f65f29b50dadcaaf12e257e
Edu Martins <eduardogabriel.rj38@gmail.com>
2024-09-27 16:53:10 -0300

    Update trabFisica.c

--- trabFisica.c
+++ trabFisica.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+typedef struct{
+    double coeficiente;
+    int expoente;
+}Fator;
+
+void derivar(Fator fator){
+    Fator resultado;
+
+    resultado.coeficiente = fator.coeficiente * fator.expoente;    
+    resultado.expoente = fator.expoente - 1;
+
+    printf("%.1lfx", resultado.coeficiente);
+
+    if(resultado.expoente > 1){
+        printf("^%d", resultado.expoente);
+    }
+
+    if(resultado.expoente > 1){
+        printf(" + ");
+    }
+
+}
+
 int main(){
-    int aleatorio[50][50], input = 0;
-    for(int cont = 0; cont < 50; cont++){
-        for(int cont2 = 0; cont2 < 50; cont2++){
-            aleatorio[cont][cont2] = rand();
-            input += aleatorio[cont][cont2];}}
-    printf("Seu resultado: %d", input);
-    return 0;}
+
+    printf("qual o grau da funcao?\n");
+    int grau;
+    scanf("%d", &grau);
+
+    if(grau == 0){
+        printf("resultado : 0");
+    }
+
+    Fator fatores[grau];
+
+    for(int cont = grau; cont > -1; cont--){
+        printf("insira o coeficiente\n");
+        scanf("%lf", &fatores[cont].coeficiente);
+        fatores[cont].expoente = cont;
+    }
+
+    printf("\n\n\n\n\n");
+    printf("a funcao: ");
+    for(int cont = grau; cont > -1; cont--){
+        printf("%.1lfx^%d + ", fatores[cont].coeficiente, cont);
+    }
+
+    printf("\ntem como derivada: ");
+
+    for(int cont = grau; cont > 1; cont--){
+        derivar(fatores[cont]);
+    }
+
+    printf(" + %.1lf", fatores[1].coeficiente);
+
+    printf("\n\n\n\n");
+    
+    return 0;
+}
